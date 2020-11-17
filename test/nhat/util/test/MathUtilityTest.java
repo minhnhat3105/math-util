e193477b508071ed84ff1781f587d4f051025cf5
minhnhat3105 <testSWE69@gmail.com>
2020-11-17 15:50:30 +0700

    add JUnit library

---
+++ test/nhat/util/test/MathUtilityTest.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nhat.util.test;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Gwi99
+ */
+public class MathUtilityTest {
+    
+    public MathUtilityTest() {
+    }
+    
+}
